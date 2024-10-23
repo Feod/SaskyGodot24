09387383384c42a7e30a8600da84dced165db940
Feod <miikkaharjuntausta@gmail.com>
2024-10-23 18:54:31 +0300

    Import `PlayerManager` and update `FollowPlayersCenter.gd` to follow players' center
    * Import `PlayerManager` from `PlayerManager.gd`
    * Add `lerp_speed` variable to control the speed of the lerp
    * Update `_ready` function to set the initial position to the players' center
    * Update `_process` function to use `lerp` to move towards the players' center position

--- FollowPlayersCenter.gd
+++ FollowPlayersCenter.gd
@@ -1,12 +1,17 @@
 extends Node
 
-# TODO: Make into script that allows objects like camera to follow player center with lerp
+# Import PlayerManager from PlayerManager.gd
+import "res://PlayerManager.gd"
+
+# Variable to control the speed of the lerp
+@export var lerp_speed: float = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	# Set the initial position to the players' center
+	position = PlayerManager.instance.playersCenter
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Use lerp to move towards the players' center position
+	position = position.lerp(PlayerManager.instance.playersCenter, lerp_speed)
